212c17cb46b78269ea3d3c98cf476329d462bbc3
Giuseppe Cinque <gfcinque@inwind.it>
2022-11-14 12:40:06 +0100

    Cap.8 DOCKER

---
+++ docker/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Questo è un commento
+echo Hello World # Anche questo è un commento!
+cd /home/giuseppe/Documents/java/workspace/git/learn-microservices-spring-boot-2e-my/multiplication
+sudo ./mvnw spring-boot:build-image -DskipTests
+cd /home/giuseppe/Documents/java/workspace/git/learn-microservices-spring-boot-2e-my/gamification
+sudo ./mvnw spring-boot:build-image -DskipTests
